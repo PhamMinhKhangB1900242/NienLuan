68f085125d9625b661029871f7dd316da33aa166
PhamMinhKhangB1900242 <101611865+PhamMinhKhangB1900242@users.noreply.github.com>
2023-04-20 21:57:09 +0700

    Delete public directory

--- public/layout/footer.php
+++
@@ -1,9 +0,0 @@
-
-<footer id="footer" class="footer">
-        <p>PokéShop Hân Hạnh Phục Vụ Bạn</p>
-        <a class="top" href="index.html"></a>
-        <p>Liên hệ chúng tui qua
-            <a href="fb.com"><i class="fa fa-facebook-f" ></i></a>
-            <a href="twitter.com"><i class="fa fa-twitter" ></i></a> hoặc địa chỉ email <a href=""><i class="fas fa-mail-bulk"></i></a>
-        </p>
-</footer>
